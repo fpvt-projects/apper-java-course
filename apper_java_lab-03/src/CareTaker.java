62d550140df9ec1f97a0542174f17f1d68615568
Francis Paolo <francispaolo.talan@gmail.com>
2022-03-12 04:37:11 +0800

    Lab 3-Initial Commit

---
+++ apper_java_lab-03/src/CareTaker.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CareTaker {
+   private static List<Memento> mementoList = new ArrayList<>();
+
+    public void add(Memento state){
+        mementoList.add(state);
+    }
+
+    public Memento get(){
+        return mementoList.get(mementoList.size() - 2);
+    }
+
+    public Memento specificGet(int index){
+        return mementoList.get(index);
+    }
+
+}
---
+++ apper_java_lab-03/src/Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Main {
+    final private static Logger logger = Logger.getLogger(Main.class.getName());
+    static Scanner scan = new Scanner(System.in);
+    static Originator originator = new Originator();
+    static CareTaker careTaker = new CareTaker();
+
+    public static void main(String[] args) {
+        initialData();
+    }
+
+    public static void init() {
+        System.out.println("================================");
+        System.out.println("[1] - Input");
+        System.out.println("[2] - Undo");
+        System.out.println("[3] - States");
+        System.out.println("[4] - Exit");
+        System.out.println("================================");
+        System.out.print("Response: ");
+        int input = scan.nextInt();
+        scan.nextLine();
+
+        switch (input) {
+            case 1 -> InputData();
+            case 2 -> retrieveState();
+            case 3 -> retrieveAll();
+            case 4 -> logger.info("Program Terminated");
+
+        }
+    }
+
+    static void initialData(){
+        System.out.print("Input: ");
+        String firstInput = scan.nextLine();
+        saveInput(firstInput);
+    }
+
+    static void InputData(){
+        System.out.print("Input: " + originator.getState() + " ");
+        String firstInput = scan.nextLine();
+        String givenState = originator.getState().toString();
+        String finalInput = givenState + " " + firstInput;
+        saveInput(finalInput);
+    }
+
+    public static void saveInput(String input){
+        originator.setState(input);
+        careTaker.add(originator.saveStateToMemento());
+        logger.info("State Saved: " + originator.getState());
+        System.out.println("\n");
+        init();
+    }
+
+    public static void retrieveState(){
+        originator.getStateFromMemento(careTaker.get());
+        System.out.print("Input: " + originator.getState() + " ");
+        String firstInput = scan.nextLine();
+        saveInput(firstInput);
+    }
+
+    public static void retrieveAll(){
+        originator.getStateFromMemento(careTaker.specificGet(0));
+        logger.info("State 1: " + originator.getState());
+        originator.getStateFromMemento(careTaker.specificGet(1));
+        logger.info("State 2: " + originator.getState());
+        originator.getStateFromMemento(careTaker.specificGet(2));
+        logger.info("State 3: " + originator.getState());
+        originator.getStateFromMemento(careTaker.specificGet(3));
+        logger.info("State 4: " + originator.getState());
+    }
+}
